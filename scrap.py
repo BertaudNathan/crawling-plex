744fd7efdbe4a86c4718f4c315c846d0411bd0e0
lremi <remi.leroi@ynov.com>
2023-11-17 10:33:31 +0100

    Ouais la team

--- scrap.py
+++ scrap.py
@@ -17,7 +17,6 @@ browser.get('https://thepiratebay.org/index.html')
 
 search_bar = browser.find_element(By.CSS_SELECTOR, "input")
 arguments = sys.argv[1:]
-print(arguments + " python")
 search_bar.send_keys(arguments)
 search_bar.send_keys(Keys.ENTER)
 
